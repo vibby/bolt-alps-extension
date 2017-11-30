06e2cb47d35bec1ea9cd6fd4d18113b219573bbe
vibby <vincent@beauvivre.fr>
2017-11-30 08:48:55 +0100

    Add other dates than sabbath in dates import

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -3,7 +3,6 @@
 namespace Bolt\Extension\Vibby\Alps\Command;
 
 use Bolt\Legacy\Content;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -51,77 +50,90 @@ class ImportCommand extends BaseCommand
             $date = new \DateTime($dateSource);
             preg_match('#<a href="/?([^/]*)?/([^"]*)">(.*)</a>#', $page['Titre'], $matches);
             list($fullMatch, $type, $slug, $title) = $matches;
-            if (!$title) {
-                die('stop');
-            }
-            $type = ($type == 'videos') ? 'predication' : $type;
-            if ($type == 'videos') {
-                $type = 'predication';
-            }
-            if ($type == 'demande') {
-                continue;
-            }
-            if ($type == 'newsletter') {
-                continue;
-            }
-            $record = $this->app['storage']->getEmptyContent(($type) ? $type : 'pages');
-            preg_match('#src="https://www.nantes-adventiste.com/sites/default/files/([^"]*)".*( alt="([^"]*)")?#', $page['Image'], $matches);
-            $imagePath = isset($matches[1]) ? $matches[1] : null;
-            $imageAlt = isset($matches[3]) ? $matches[3] : null;
-            preg_match('#"(https://www.youtube.[^"]*)"#', isset($page['Vidéo']) ? $page['Vidéo'] : null, $matches);
-            $videoUrl = isset($matches[1]) ? $matches[1] : null;
             if ($type == 'planning') {
                 if (in_array($date->format('Y-m-d'), $planningDone)) {
                     continue;
                 }
                 $planningDone[] = $date->format('Y-m-d');
-            }
-            $record->setValues([
-                'title' => $type == 'planning' ? $date->format('Y-m-d') : $title,
-                'slug' => $type == 'planning' ? $date->format('Y-m-d') : $slug,
-                'body' => $page['Contenu'],
-                'video' => serialize([
-                    'url' => $videoUrl,
-                    'width' => 480,
-                    'height' => 270,
-                    'authorname' => 'Eglise adventiste du 7e Jour de Nantes',
+                if (isset($page['Autres'])) {
+                    $this->insertDates($page['Autres'], $date);
+                }
+                $record = $this->app['storage']->getEmptyContent('planning');
+                $record->setValues([
+                    'title' => $date->format('Y-m-d'),
+                    'slug' => $date->format('Y-m-d'),
+                    'status' => 'published',
+                    'datepublish' => $date ? $date->format('Y-m-d 00:00:00') : null,
+                    'status' => 'published',
+                    'elders' => isset($page['Anciens référents']) ? $page['Anciens référents'] : null,
+                    'afternoon' => isset($page['Après-midi']) ? $page['Après-midi'] : null,
+                    'sabbath_school_mission' => isset($page['Catéchèse Bulletin']) ? $page['Catéchèse Bulletin'] : null,
+                    'sabbath_school_prayer' => isset($page['Catéchèse Prière']) ? $page['Catéchèse Prière'] : null,
+                    'sabbath_school_presidence' => isset($page['Catéchèse Présidence']) ? $page['Catéchèse Présidence'] : null,
+                    'pray_cell' => isset($page['Cellule de prière']) ? $page['Cellule de prière'] : null,
+                    'sing_leader' => isset($page['Conducteur chant']) ? $page['Conducteur chant'] : null,
+                    'deacon' => isset($page['Diaconat']) ? $page['Diaconat'] : null,
+                    'slideshow' => isset($page['Diaporamas']) ? $page['Diaporamas'] : null,
+                    'presidence' => isset($page['Estrade']) ? $page['Estrade'] : null,
+                    'kids_story' => isset($page['Histoire enfants']) ? $page['Histoire enfants'] : null,
+                    'bible_reader' => isset($page['Lecture biblique']) ? $page['Lecture biblique'] : null,
+                    'belief_reader' => isset($page['Lecture croyances']) ? $page['Lecture croyances'] : null,
+                    'musician' => isset($page['Musique']) ? $page['Musique'] : null,
+                    'cleaner' => isset($page['Ménage']) ? $page['Ménage'] : null,
+                    'pastoral_pray' => isset($page['Prière pastorale']) ? $page['Prière pastorale'] : null,
+                    'predication' => isset($page['Prédication']) ? $page['Prédication'] : null,
+                    'pray_meeting' => isset($page['Réunion de prière']) ? $page['Réunion de prière'] : null,
+                    'sonorisation' => isset($page['Sonorisation']) ? $page['Sonorisation'] : null,
+                    'special' => isset($page['Spécial']) ? $page['Spécial'] : null,
+                ]);
+                $record->set('datecreated', $date ? $date->format('Y-m-d 00:00:00') : null);
+            } else {
+                if (!$title) {
+                    die('stop');
+                }
+                $type = ($type == 'videos') ? 'predication' : $type;
+                if ($type == 'videos') {
+                    $type = 'predication';
+                }
+                if ($type == 'demande') {
+                    continue;
+                }
+                if ($type == 'newsletter') {
+                    continue;
+                }
+                $record = $this->app['storage']->getEmptyContent(($type) ? $type : 'pages');
+                preg_match('#src="https://www.nantes-adventiste.com/sites/default/files/([^"]*)".*( alt="([^"]*)")?#', $page['Image'], $matches);
+                $imagePath = isset($matches[1]) ? $matches[1] : null;
+                $imageAlt = isset($matches[3]) ? $matches[3] : null;
+                preg_match('#"(https://www.youtube.[^"]*)"#', isset($page['Vidéo']) ? $page['Vidéo'] : null, $matches);
+                $videoUrl = isset($matches[1]) ? $matches[1] : null;
+                $record->setValues([
                     'title' => $title,
-                ]),
-                'image' => serialize([
-                    'file' => $imagePath,
-                    'alt' => $title,
-                ]),
-                'status' => 'published',
-                'datepublish' => $date ? $date->format('Y-m-d 00:00:00') : null,
-                'status' => 'published',
-                'elders' => isset($page['Anciens référents']) ? $page['Anciens référents'] : null,
-                'afternoon' => isset($page['Après-midi']) ? $page['Après-midi'] : null,
-                'other' => isset($page['Autres']) ? $page['Autres'] : null,
-                'sabbath_school_mission' => isset($page['Catéchèse Bulletin']) ? $page['Catéchèse Bulletin'] : null,
-                'sabbath_school_prayer' => isset($page['Catéchèse Prière']) ? $page['Catéchèse Prière'] : null,
-                'sabbath_school_presidence' => isset($page['Catéchèse Présidence']) ? $page['Catéchèse Présidence'] : null,
-                'pray_cell' => isset($page['Cellule de prière']) ? $page['Cellule de prière'] : null,
-                'sing_leader' => isset($page['Conducteur chant']) ? $page['Conducteur chant'] : null,
-                'deacon' => isset($page['Diaconat']) ? $page['Diaconat'] : null,
-                'slideshow' => isset($page['Diaporamas']) ? $page['Diaporamas'] : null,
-                'presidence' => isset($page['Estrade']) ? $page['Estrade'] : null,
-                'kids_story' => isset($page['Histoire enfants']) ? $page['Histoire enfants'] : null,
-                'bible_reader' => isset($page['Lecture biblique']) ? $page['Lecture biblique'] : null,
-                'belief_reader' => isset($page['Lecture croyances']) ? $page['Lecture croyances'] : null,
-                'musician' => isset($page['Musique']) ? $page['Musique'] : null,
-                'cleaner' => isset($page['Ménage']) ? $page['Ménage'] : null,
-                'pastoral_pray' => isset($page['Prière pastorale']) ? $page['Prière pastorale'] : null,
-                'predication' => isset($page['Prédication']) ? $page['Prédication'] : null,
-                'pray_meeting' => isset($page['Réunion de prière']) ? $page['Réunion de prière'] : null,
-                'sonorisation' => isset($page['Sonorisation']) ? $page['Sonorisation'] : null,
-                'special' => isset($page['Spécial']) ? $page['Spécial'] : null,
-            ]);
-            $record->set('datecreated', $date ? $date->format('Y-m-d 00:00:00') : null);
-            if ($page['Type'] == 'Média') {
-                $record->setTaxonomy('pages', 'media');
-            }
-            if ($page['Type'] == 'Article') {
-                $record->setTaxonomy('pages', 'news');
+                    'slug' => $slug,
+                    'date' => $date ? $date->format('Y-m-d 00:00:00') : null,
+                    'body' => $page['Contenu'],
+                    'video' => serialize([
+                        'url' => $videoUrl,
+                        'width' => 480,
+                        'height' => 270,
+                        'authorname' => 'Eglise adventiste du 7e Jour de Nantes',
+                        'title' => $title,
+                    ]),
+                    'image' => serialize([
+                        'file' => $imagePath,
+                        'alt' => $title,
+                    ]),
+                    'status' => 'published',
+                    'datepublish' => $date ? $date->format('Y-m-d 00:00:00') : null,
+                    'status' => 'published',
+                ]);
+                $record->set('datecreated', $date ? $date->format('Y-m-d 00:00:00') : null);
+                if ($page['Type'] == 'Média') {
+                    $record->setTaxonomy('pages', 'media');
+                }
+                if ($page['Type'] == 'Article') {
+                    $record->setTaxonomy('pages', 'news');
+                }
             }
 
             $this->app['storage']->saveContent($record);
@@ -129,6 +141,78 @@ class ImportCommand extends BaseCommand
         }
     }
 
+    private function insertDates($otherContent, $dateSabbath)
+    {
+        $newContent = '';
+        $date = null;
+        dump($otherContent);
+        foreach(preg_split("/<[^>]>/", $otherContent) as $line){
+            $line = strip_tags($line);
+            if (!$line) {
+                continue;
+            }
+            preg_match('#(\d{1,2}) (janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)#i', $line, $match);
+            dump($line);
+            if (isset($match[0])) {
+                if ($newContent && $date) {
+                    $record = $this->app['storage']->getEmptyContent('planning');
+                    $record->setValues([
+                        'title' => $date->format('Y-m-d'),
+                        'slug' => $date->format('Y-m-d'),
+                        'status' => 'published',
+                        'datepublish' => $date ? $date->format('Y-m-d 00:00:00') : null,
+                        'status' => 'published',
+                        'other' => $newContent,
+                    ]);
+                    $this->app['storage']->saveContent($record);
+                    dump($record->getTitle());
+                }
+                list($full, $dom, $month) = $match;
+                $month = strtr(
+                    $month,
+                    [
+                        'janvier' => 1,
+                        'février' => 2,
+                        'mars' => 3,
+                        'avril' => 4,
+                        'mai' => 5,
+                        'juin' => 6,
+                        'juillet' => 7,
+                        'août' => 8,
+                        'septembre' => 9,
+                        'octobre' => 10,
+                        'novembre' => 11,
+                        'décembre' => 12,
+                    ]
+                );
+                $year = $dateSabbath->format('Y');
+                if ($month = 1 && $dateSabbath->format('n') == 12) {
+                    $year++;
+                }
+                if ($month = 12 && $dateSabbath->format('n') == 1) {
+                    $year--;
+                }
+                $date = new \DateTime(sprintf('%d-%d-%d', $year, $month, $dom));
+                $newContent = '';
+            } else {
+                $newContent .= $line."\n";
+            }
+        }
+        if ($newContent && $date) {
+            dump($newContent);
+            $record = $this->app['storage']->getEmptyContent('planning');
+            $record->setValues([
+                'title' => $date->format('Y-m-d'),
+                'slug' => $date->format('Y-m-d'),
+                'status' => 'published',
+                'datepublish' => $date ? $date->format('Y-m-d 00:00:00') : null,
+                'status' => 'published',
+                'other' => $newContent,
+            ]);
+            $this->app['storage']->saveContent($record);
+            dump($record->getTitle());
+        }
+    }
 
     protected function parseCsv($file)
     {
